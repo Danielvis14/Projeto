e28779d83dd149ccdc45a25186db2c87f8c17597
Danielvis14 <danielgamerbr14@gmail.com>
2017-05-10 16:55:28 -0300

    Delete oi.sh

--- oi.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-dialog --inputbox "NOME DO USUARIO" 2>$USUARIO
-dialog --inputbox "Digite Sua Senha" 2>$SENHA
-mkdir /etc/skel/Musicas
-mkdir /etc/skel/Downloads
-mkdir /etc/skel/Documentos
-useradd -m -d/home/$USUARIO
-passwd $USUARIO
