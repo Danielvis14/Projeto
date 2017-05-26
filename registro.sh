96b1d97dc7966aed270445afdb0d836f4c437a3e
root <root@jessie.raw>
2017-05-26 18:33:08 +0000

    oi

---
+++ registro.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ dialog	--stdout --yesno "Você já tem cadastro ?" 0 0  
+
+if [ $? = 0 ]; then
+ dialog --stdout --title "Login" --inputbox "Coloque o usuário:" 0
+ dialog --stdout --title "Senha" --passwordbox "Coloque a senha:" 0 0 )
+
+
+else
+ dialog --stdout --inputbox "Digite o nome desejado:" 0 0 )
+	
+ dialog --stdout --title "Nova senha" --passwordbox "Coloque sua senha:" 0 0 )
+	$SENHA >>Users.txt
+
+ dialog --stdout --title "Login" --inputbox "Coloque o usuário" 0 0 )
+ dialog --stdout --title "Login" --passwordbox "Coloque a senha:" 0 0 )
+
+fi
+
+
